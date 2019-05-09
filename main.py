8f1a602d863ba94899a4425ce5933421add42e8e
innei <tukon479@gmail.com>
2019-05-09 15:49:26 +0800

    fix bugs.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class HexoToTypecho():
         self.port = 3306
         self.database = database
         self.files = files
+        self.connectSQL()
         # self.parse_hexo_md()
         for i in files:
             self.insert_post(i)
@@ -20,13 +21,21 @@ class HexoToTypecho():
         self.db.close()
 
     def connectSQL(self):
-        sql = pymysql.connect(host=self.host, user=self.user, password=self.passwd, database=self.database,
-                              charset='utf8')
-        return sql.cursor(), sql
+        try:
+            sql = pymysql.connect(host=self.host, user=self.user, password=self.passwd, database=self.database,
+                                  charset='utf8')
+        except pymysql.Error as e:
+            print(e)
+            quit(1)
+        else:
+            print('连接成功')
+            self.cur = sql.cursor()
+            self.db = sql
+            # self.cur.execute('ALTER TABLE typecho_metas ADD UNIQUE KEY(name,type)')
 
-    def parse_hexo_md(self,file):
+    def parse_hexo_md(self, file):
         # TODO 文件名可以作为缩略名
-        with open('_post/'+file, encoding='utf-8') as f:
+        with open('_posts/' + file, encoding='utf-8') as f:
             s = f.read()
             print('当前处理 --->  ', file)
 
@@ -36,36 +45,49 @@ class HexoToTypecho():
         date = re.search(r'date: (.*?)\n', s, re.S).group(1)
         date = time.strptime(date, "%Y-%m-%d %H:%M:%S")
         date = int(time.mktime(date))
+        try:
+            if not re.search(r'tags:[ ]*(.*?)\n', s).group(1):
+                if re.search(r'tags:[ ]*\n(.*?)\nca', s, re.S):
+                    items = re.search(r'tags:[ ]*\n(.*?)\nca', s, re.S).group(1)
+                    tags = re.findall(r'- (.*?)\n', items)
+                else:
+                    tags = ''
+            else:
+                tags = re.search(r'tags:[ ]*(.*?)\n', s).group(1)
+        except AttributeError as e:
+            print(e)
+            tags = ''
 
-        if not re.search(r'tags: (.*?)\n', s):
-            items = re.search(r'tags:\n(.*?)\nca', s, re.S).group(1)
-            tags = re.findall(r'- (.*?)\n', items)
-        else:
-            tags = re.search(r'tags: (.*?)\n', s).group(1)
-
-        if not re.search(r'categories: ', s):
-            items = re.search(r'categories:\n(.*?)\n---', s, re.S).group(1)
-            categories = re.findall('- (.*?)\\n', items)
-        else:
-            categories = re.search(r'categories: (.*?)\n', s).group(1)
+        try:
+            if not re.search(r'categories:[ ]*(.*?)\n', s).group(1):
+                if re.search(r'categories:[ ]*\n(.*?)\n---', s, re.S):
+                    items = re.search(r'categories:[ ]*\n(.*?)\n---', s, re.S).group(1)
+                    categories = re.findall(r'- (.*?)\n', items)
+                else:
+                    categories = ''
+            else:
+                categories = re.search(r'categories:[ ]*(.*?)\n', s).group(1)
+        except AttributeError as e:
+            print(e)
+            categories = ''
         # 正文提取
         post = re.search(r'---\n\n(.*?)$', s, re.S).group(1)
 
         # print((title, date, tags, categories,post))
         return (title, date, tags, categories, '<!--markdown-->' + post)
 
-    def insert_post(self,file):
+    def insert_post(self, file):
         data = self.parse_hexo_md(file)
         self.data = data
-        cur, db = self.connectSQL()
-        self.db = db
-        self.cur = cur
+        db = self.db
+        cur = self.cur
+        modified = int(time.mktime(time.localtime(os.stat('_posts/' + file).st_mtime)))
         sql = '''
-        INSERT INTO typecho_contents(title,slug, created,modified, text,type,status,allowcomment,allowfeed,authorid) VALUES (%s,%s,%s,%s,%s,'post','publish',1,1,1) 
+        INSERT INTO typecho_contents(title,slug, created,modified, text,type,status,allowComment,allowFeed,allowPing,authorId) VALUES (%s,%s,%s,%s,%s,'post','publish',1,1,1,1) 
         '''
 
         try:
-            cur.execute(sql, (data[0], file, data[1], data[1], data[4]))
+            cur.execute(sql, (data[0], file, data[1], modified, data[4]))
             db.commit()
         except Exception as e:
             print(e)
@@ -74,9 +96,9 @@ class HexoToTypecho():
     def insert_tags_category(self):
         data = self.data
         cur = self.cur
-        cur.execute('ALTER TABLE typecho_metas ADD UNIQUE KEY(name,type)')
+        # cur.execute('ALTER TABLE typecho_metas ADD UNIQUE KEY(name,type)')
         sql = '''
-        INSERT INTO typecho_metas(name,slug,type) VALUES (%s,%s,'tag') ON DUPLICATE KEY UPDATE count = count + 1
+        INSERT INTO typecho_metas(name,slug,type,count) VALUES (%s,%s,'tag',1) ON DUPLICATE KEY UPDATE count = count + 1
         '''
         # tags导入
         try:
@@ -86,15 +108,16 @@ class HexoToTypecho():
                     cur.execute(sql, (i, i))
                     self.db.commit()
             else:
-                cur.execute(sql, (data[2], data[2]))
-                self.db.commit()
+                if data[2]:
+                    cur.execute(sql, (data[2], data[2]))
+                    self.db.commit()
         except pymysql.DatabaseError as e:
             print(e)
             self.db.rollback()
 
         # category 导入
         sql = '''
-                INSERT INTO typecho_metas(name,slug,type) VALUES (%s,%s,'category') ON DUPLICATE KEY UPDATE count = count + 1
+                INSERT INTO typecho_metas(name,slug,type,count) VALUES (%s,%s,'category',1) ON DUPLICATE KEY UPDATE count = count + 1
               '''
         try:
             # (title, date, tags, categories, '<!--markdown-->' + post)
@@ -103,8 +126,9 @@ class HexoToTypecho():
                     cur.execute(sql, (i, i))
                     self.db.commit()
             else:
-                cur.execute(sql, (data[3], data[3]))
-                self.db.commit()
+                if data[3]:
+                    cur.execute(sql, (data[3], data[3]))
+                    self.db.commit()
         except pymysql.DatabaseError as e:
             print(e)
             self.db.rollback()
@@ -113,7 +137,7 @@ class HexoToTypecho():
         db = self.db
         cur = self.cur
         data = self.data
-
+        print('tag = ', data[2], 'type = ', type(data[2]), 'cet = ', data[3])
         # 映射 tags
         select_mid = '''
                 SELECT mid FROM typecho_metas WHERE name = %s AND type = %s
@@ -127,7 +151,7 @@ class HexoToTypecho():
 
         try:
             cur.execute(select_cid, (data[0]))
-            
+
             cid = cur.fetchall()[0][0]  # 获取 cid
 
             if isinstance(data[2], list):
@@ -141,14 +165,17 @@ class HexoToTypecho():
             else:
                 cur.execute(select_mid, (data[2], 'tag'))
                 tu = cur.fetchall()
-                # print('mid = ', tu[0][0])  # mid 获取
+                print('mid = ', tu)  # mid 获取
                 mid = tu[0][0]
                 cur.execute(add_relationship, (cid, mid))
         except pymysql.DatabaseError as e:
             print(e)
             db.rollback()
+        except IndexError as e:
+            print('不能建立关系', data[2])
+            return
 
-        # categories
+            # categories
         # (title, date, tags, categories, '<!--markdown-->' + post)
         try:
             if isinstance(data[3], list):
@@ -168,9 +195,12 @@ class HexoToTypecho():
         except pymysql.DatabaseError as e:
             print(e)
             db.rollback()
+        except IndexError as e:
+            print('不能建立关系', data[3])
+            return
 
 
 if __name__ == '__main__':
-    files = [f for f in os.listdir('_post') if not f.startswith('.')]
+    files = [f for f in os.listdir('_posts') if not f.startswith('.')]
     print('总有', files)
     HexoToTypecho(host='', user='', database='', files='', passwd='')
